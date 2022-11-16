400139efb1b3c66da9e6c52fff4464d8a36a134b
Rowan <116010023+Rowan7@users.noreply.github.com>
2022-11-16 11:12:47 +0000

    abstracted formatBags() from main function.

--- main.sh
+++ main.sh
@@ -30,90 +30,13 @@ TIMEDOUT=125
 
 
 formatBags() {
-
-# FORMAT BAG NAMES HERE BEFORE THEY'RE POPULATED WITH .JSON
-# Clean up and format bag files in target directory
-echo "Formatting Bags"
-
-declare -A keywordCorrection=( # Declare an associative array containing known keyword targets and their respective corrections.
-                               # Note: associative arrays are non-indexable/orderable as they are represented as a hash function.
-    ["pearce_"]="frognall_" \
-    ["peasgood_"]="frognall_" \
-    ["gs_"]="ely_" \
-    ["beet_"]="sugarbeet_" \
-    ["leak_"]="leek_" \
-    ["none_none_auto_none_"]="wheat_papley_" \
-    
-
-)
-
-# Add each correction in front of keyword, then delete all necessary keywords afterwards in seperate loop (this is because some keywords don't want replacing e.g pearce, but delete all misspellings of leek.)
-index=0
-for keyword in ${!keywordCorrection[@]}; do
-    correction=${keywordCorrection[${keyword}]}
-  # echo variables 
-    echo "Target Keyword: $keyword"
-    echo "Correction: $correction"
-    echo "Index: $index"
-
-  # go through each item in the above array, if it contains keyword: check if it already contains correction
-  # if it already contains correction, pass, else: add correction infront of the first instance of keyword found
-    if [[ "$keyword" == "beet_" ]]; then
-
-        find ${A_TARGET_BAG_DIR} -type f \( -iname *"beet"* -and -not -iname *"sugarbeet"*  \) | while read line;
-        do
-            if [[ "$line" != *"$correction"* ]]; then
-                newFileName=${line/$keyword/${correction}${keyword}}
-                echo "'$keyword' Keyword found in $line, adding '$correction'"
-                mv $line $newFileName
-                echo "New File Name: $newFileName"
-            else
-                echo "$line already contains $correction, not renaming."
-            fi
-        done
-        echo "==================================="
-    else
-        find ${A_TARGET_BAG_DIR} -type f \( -iname *"$keyword"* \) | while read line;
-        do
-            if [[ "$line" != *"$correction"* ]]; then
-                newFileName=${line/$keyword/${correction}${keyword}}
-                echo "'$keyword' Keyword found in $line, adding '$correction'"
-                mv $line $newFileName
-                echo "New File Name: $newFileName"
-            else
-                echo "$line already contains $correction, not renaming."
-            fi
-        done
-        echo "==================================="
-    fi
-    ((index++))
-done
-
-removalArray=("none_" "leak_" "_beet") #ONLY REMOVE BEET_ IF THERE ISN'T SUGAR INFRONT OF IT
-# if there's any instances of any items in removalArray in the filename, remove them.
-# If it finds an instance of "beet_", only remove it if it's not got sugar preceeding it.. BUG
-# don't add instances of sugarbeet when beet_ is already preceeded by sugar.
-# don't remove instances of beet_ if it is preceeded by sugar <--- PERFECT SOLUTION
-# My solution is imperfect, it will only get rid of instances of _beet_ | this won't work correctly when filename starts with "beet_frognall_pearce_RGB.bag"
-for keyword in ${removalArray[@]}; do 
-    find ${A_TARGET_BAG_DIR} -type f \( -iname *"${keyword}"* \) | while read line;
-    do 
-        newFileName=${line//${keyword}/}
-        echo "'${keyword}' keyword found in $line, Removing.."
-        mv $line $newFileName
-    done
-done
-
-
-
-
+# Format bags and tidy them up before they are used elsewhere.
+$A_PROGRAM_HOME/formatBags.sh $_A_TARGET_BAG_DIR
 }
 
 jsonPopulate() {
-
 # Populate with .json files and add key values for each detected attribute.
 $A_PROGRAM_HOME/jsonPopulate.sh $A_TARGET_BAG_DIR
-
 }
 
 
@@ -156,7 +79,6 @@ echo "FILE EXTRACTION COMPLETE"
 }
 
 formatBags
-
-# jsonPopulate()
-# extract()
+jsonPopulate
+extract
 
